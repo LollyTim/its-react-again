83153b87c144e3d280cb5252e38ffd956ffe7429
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-20 00:31:10 +0100

    Learning more

--- tut1/src/App.js
+++ tut1/src/App.js
@@ -2,6 +2,24 @@ import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
+
   return (
     <div className="App">
       <Header />
