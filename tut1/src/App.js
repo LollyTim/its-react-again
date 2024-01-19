8c0083ac21fcbe79ec127f13fa5ec8fe4f036886
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-20 00:37:57 +0100

    Learning more

--- tut1/src/App.js
+++ tut1/src/App.js
@@ -1,6 +1,7 @@
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
+import { useState } from "react";
 function App() {
   const [items, setItems] = useState([
     {
@@ -20,11 +21,29 @@ function App() {
     },
   ]);
 
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
   return (
     <div className="App">
       <Header />
-      <Content />
-      <Footer />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
