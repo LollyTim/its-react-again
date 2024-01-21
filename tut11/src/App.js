f9f4e6d6bd3b46dadb9b4d185a2a7a669e479feb
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-21 22:50:39 +0100

    doing what i love

--- tut11/src/App.js
+++ tut11/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
-
 import { useState } from "react";
 import ColorBox from "./Colorbox";
 
 const App = () => {
   const [bgColor, setBgColor] = useState("");
+  const [hexValue, setHexValue] = useState("");
+  const [TextColor, setTextColor] = useState(true);
 
   return (
     <div className="App">
-      <ColorBox bgColor={bgColor} setBgColor={setBgColor} />
+      <ColorBox
+        bgColor={bgColor}
+        setBgColor={setBgColor}
+        hexValue={hexValue}
+        setHexValue={setHexValue}
+        TextColor={TextColor}
+        setTextColor={setTextColor}
+      />
     </div>
   );
 };
--- tut11/src/Colorbox.jsx
+++ tut11/src/Colorbox.jsx
@@ -1,20 +1,45 @@
 import React from "react";
+import colorNames from "colornames";
 
-const ColorBox = ({ bgColor, setBgColor }) => {
+const ColorBox = ({
+  bgColor,
+  setBgColor,
+  hexValue,
+  setHexValue,
+  TextColor,
+  setTextColor,
+}) => {
   return (
     <div>
-      <div className="colorbox" style={{ backgroundColor: bgColor }}>
-       <p>{bgColor ? <p> {bgColor}</p> : <p>Empty Text</p>}</p>
+      <div
+        className="colorbox"
+        style={{ backgroundColor: bgColor, color: TextColor ? "#000" : "#fff" }}
+      >
+        <p>{bgColor ? bgColor : "Empty Text"}</p>
+        <p>{hexValue ? hexValue : null}</p>
       </div>
-      <input
-        className="addcolorinput"
-        type="text"
-        id="search"
-        role="searchbox"
-        placeholder="Add color name"
-        value={bgColor}
-        onChange={(e) => setBgColor(e.target.value)}
-      />
+      <form action="" onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="Add Color Name:"></label>
+        <input
+          className="addcolorinput"
+          type="text"
+          id="search"
+          role="searchbox"
+          placeholder="Add color name"
+          value={bgColor}
+          onChange={(e) => {
+            setBgColor(e.target.value);
+            setHexValue(colorNames(e.target.value));
+          }}
+        />
+      </form>
+      <button
+        type="button"
+        className="togglebutton"
+        onClick={() => setTextColor(!TextColor)}
+      >
+        Toggle Text Color
+      </button>
     </div>
   );
 };
