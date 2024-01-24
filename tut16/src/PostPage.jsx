0274459d4014121795cd527ef5148f442a2129dd
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-24 23:28:04 +0100

    upATED

--- tut16/src/PostPage.jsx
+++ tut16/src/PostPage.jsx
@@ -17,6 +17,16 @@ const PostPage = ({posts, handleDelete}) => {
               </button>
             </>
           }
+          {!post && 
+              <>
+                <h2>Post Not Found</h2>
+                <p>Well, that's disappointing.</p>
+                <p>
+                  <Link to={"/"}>Visit our Home page</Link>
+                </p>
+              </>
+          
+          }
         </article>
     </main>
   )
