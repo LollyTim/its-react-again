7df592c8b7c39c645b776a3186d33dd84f462d3a
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-24 23:22:38 +0100

    doings

--- tut16/src/Post.jsx
+++ tut16/src/Post.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Post = ({post}) => {
   return (
     <article className='post'>
-        <Link to={`/post/${post.id}`} >
-        <h2>{post.id} {post.title}</h2>
+        <Link to={`/posts/${post.id}`} >
+        <h2>{post.title}</h2>
         <p className='postDate'>{post.datetime}</p>
         </Link>
         <p className='postBody'>
--- tut16/src/PostPage.jsx
+++ tut16/src/PostPage.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { useParams, Link } from 'react-router-dom'
 
 const PostPage = ({posts, handleDelete}) => {
-  const {id} = useParams();
-  const post = post.find(post => (post.id).toString() === id);
+  const { id } = useParams();
+  const post = posts.find(post => (post.id).toString() === id);
   return (
     <main>
-        <article className='post'>
+        <article className='PostPage'>
           {post && 
             <>
               <h2>{post.title}</h2>
               <p className='postDate'>{post.datetime}</p>
               <p className='postBody'>{post.body}</p>
-              <button onClick={() => handleDelete(post.id)}>
+              <button className='' onClick={() => handleDelete(post.id)}>
                 Delete Post
               </button>
             </>
