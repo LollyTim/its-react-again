8e0204849c71dcc65f061b32a80d29f33a416e77
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-02-07 23:18:08 +0100

    doing hard things

--- reduxtut2/src/features/posts/AddPostForm.jsx
+++ reduxtut2/src/features/posts/AddPostForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-// import { nanoid } from "@reduxjs/toolkit";
+import { nanoid } from "@reduxjs/toolkit";
 import { postAdded } from "./postsSlice";
 
 const AddPostForm = () => {
